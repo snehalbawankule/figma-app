cf12882da55cf8abd7c131d8d5a8660bbdaed45d
snehalbawankule <snehalbawankule2000@gmail.com>
2022-12-01 18:20:52 +0530

    commit

--- src/Registration/Registration.js
+++ src/Registration/Registration.js
@@ -3,7 +3,7 @@ import "./Registration.css";
 import {useNavigate } from 'react-router-dom';
 import { formValidate } from "../validate/formValidate";
 import Statusbar from '../assets/images/Statusbar.png';
-const Registration=()=> {
+function Registration() {
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log(userInfo);
@@ -34,7 +34,7 @@ const Registration=()=> {
         <input type="email" className="input-box" name="email" placeholder="Enter your email"  onChange={handleChange} value={userInfo.email} required />
         <input type="password" className="input-box" id="pass10"name="password" value={userInfo.password}  onChange={handleChange} pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" placeholder="Enter Password" required/>
         <input type="password"  className="input-box" id="pass20" name="password1" onChange={handleChange}  value={userInfo.password1}placeholder="Confirm Password" required/>
-        <input type="submit" className="btn01" value="Get Started" onClick={formValidate} />
+        <input type="submit" className="btn01" value="Get Started" onClick={function(){formValidate(); navLogin()}} />
         <h4 className="cls-Robo" style={{fontSize:18}}>Already have an account? <input type="button" className="link" value="Login" onClick={navLogin}></input></h4>
       </form>
     </div></>
